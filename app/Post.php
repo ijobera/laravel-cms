83b8a2a1ed562390d2f86137eed1231069c8d6e3
Isiah Obera <ijobera@springvalley.tech>
2018-09-29 21:56:32 +0800

    Display all post to the blog index

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function getImageUrlAttribute($value)
+    {
+        $imageUrl = "";
+        if ( ! is_null ($this->img))
+        {
+            $imagePath = public_path() . "/img/" . $this->img;
+            if (file_exists($imagePath)) $imageUrl = asset("img/" . $this->img);
+        }
+
+        return $imageUrl;
+    }
 }
